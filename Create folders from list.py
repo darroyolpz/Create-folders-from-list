63a42f90429b845f6a0276e0f1cb78bef5cdde6e
darroyolpz <darroyolpz@users.noreply.github.com>
2019-09-20 11:09:25 +0200

    Script

---
+++ Create folders from list.py
@@ -0,0 +1,24 @@
+import os
+import pandas as pd
+
+# Open file with the orders to be created
+excel_file = 'Orders.xlsx'
+df = pd.read_excel(excel_file)
+df['Orders'] = df['Orders'].astype(str)
+
+# Loop and create the folders
+for order in df['Orders']:
+	# Nested subfolders to be created
+	subfolders = ['DVF', 'Control & wiring diagrams']
+
+	try:
+		for subfolder in subfolders:
+			folder_to_create = order + '/' + subfolder
+
+			# Create new folder in the path where the script is located
+			os.makedirs(folder_to_create)
+			print('Folder', folder_to_create, 'created')
+		print('\n')
+	except:
+		print('Warning! Folder', folder_to_create, 'already created')
+		print('\n')
